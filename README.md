0dc66d5de9bc0a9875e5bf007f287cde3eabc11d
Marcus Rangel <marcus.vbrangel@gmail.com>
2021-04-27 23:19:57 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ignite-react-componentizando-aplicacao
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,41 @@
+import { Button } from './Button';
+import React, { InputHTMLAttributes } from 'react';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface ComponentProps extends InputHTMLAttributes<HTMLInputElement> {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar(props: ComponentProps) {
+ 
+  const { genres, selectedGenreId, handleClickButton } = props;
+
+  return (
+
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+
+  );
+
+}
